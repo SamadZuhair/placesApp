413617767d952c42751534b96c30cc128dca5a0d
Vito Muhammed <mcvitobaghdadioscp@gmail.com>
2022-04-08 16:00:27 +0300

    make the popUp VC and and segue it

---
+++ placesApp/popUpViewController.swift
@@ -0,0 +1,35 @@
+//
+//  popUpViewController.swift
+//  placesApp
+//
+//  Created by Vito Muhammed  on 4/8/22.
+//
+
+import UIKit
+
+class popUpViewController: UIViewController {
+
+    @IBOutlet weak var placeName: UITextField!
+    @IBOutlet weak var descripton: UITextView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    @IBAction func save(_ sender: UIButton) {
+        print("saving")
+        dismiss(animated: true)
+    }
+    
+}
